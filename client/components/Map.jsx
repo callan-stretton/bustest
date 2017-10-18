ce46c97ab4971b43a6218083cca8a94c8fa4d316
student <>
2017-10-18 20:04:28 +1300

    added route 14

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -9,6 +9,8 @@ import bus3 from '../../server/bus-routes/bus3'
 import bus4 from '../../server/bus-routes/bus4'
 import bus6 from '../../server/bus-routes/bus6'
 import bus7 from '../../server/bus-routes/bus7'
+import bus17 from '../../server/bus-routes/bus17' // not correct route ?
+import bus14 from '../../server/bus-routes/bus14'
 import bus24 from '../../server/bus-routes/bus24'
 import bus43 from '../../server/bus-routes/bus43'
 
@@ -137,7 +139,7 @@ export default class Map extends React.Component {
     })
     // let coords = busStation[this.props.busNumber]
 
-    let servicePathCoordinates = bus43
+    let servicePathCoordinates = bus14
     let servicePath = new google.maps.Polyline({
       path: servicePathCoordinates,
       geodesic: true,
