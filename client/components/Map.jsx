55cec1fc314e2bfd0555327468b5cec7a404481c
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2017-11-21 13:11:08 +1300

    added a ternary operator to differentiate bus and train icons on map

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -158,7 +158,7 @@ export default class Map extends React.Component {
         },
         map: this.map,
         icon: {
-          url: (service.HasStarted === false) ? './images/bus-icon-not-in-service.png' : (service.Direction === 'Inbound') ? './images/bus-icon-inbound.png' : './images/bus-icon-outbound.png',
+          url: (isNaN(service.ServiceID) === false) ? (service.HasStarted === false) ? './images/bus-icon-not-in-service.png' : (service.Direction === 'Inbound') ? './images/bus-icon-inbound.png' : './images/bus-icon-outbound.png' : (service.HasStarted === false) ? './images/train-icon-not-in-service.png' : (service.Direction === 'Inbound') ? './images/train-icon-inbound.png' : './images/train-icon-outbound.png',
           scaledSize: new google.maps.Size(30, 30)
         },
         title: (service.BehindSchedule === true) ? 'Running ' + moment("1900-01-01 00:00:00").add(service.DelaySeconds, 'seconds').format("mm:ss") + ' minutes late' : 'On Time'
